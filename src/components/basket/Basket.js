bcb4c67112c3fbdb6cab4b61fdae7dbd795ee95d
AzatbekovaDev <azatbekova.ya@gmail.com>
2023-02-06 23:21:19 +0600

    add basket

--- src/components/basket/Basket.js
+++ src/components/basket/Basket.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import "./Basket.scss"
+
 const Basket = () => {
     return (
-        <div>
+        <>
+            <div id="basket">
+                <div className="basket">
+                    <h1 className="basket--order">Оформление товара</h1>
+                    <div className="basket--info">
+                        <div className="basket--info__about">
+                            <h1>Контактные данные</h1>
+                            <div className="basket--info__about--inputs">
+                                <input className="input" type="text" placeholder="Имя*"/>
+                                <input className="input" type="text" placeholder="Фамилия*"/>
+                                <input className="input" type="text" placeholder="E-Mail*"/>
+                                <input className="input" type="text" placeholder="Телефон* "/>
+                            </div>
+                            <h2>Оплата</h2>
+                            <div>
+                                <input className="basket--info__about--pay" type="checkbox" id="check" name="tabs"/>
+                                <label className="basket--info__about--check" htmlFor="check">Оплачу наличными при
+                                    получении заказа</label>
+                            </div>
+                            <h3>Оплата с банковской картой через <span>PayBox</span></h3>
+                            <button>Оплатить</button>
+                        </div>
 
-        </div>
+                        <div className="basket--info__choose">
+
+                            <div>
+                                <h1>Доставка</h1>
+                                <small>Выберите удобный способ доставки для этого заказа.</small>
+                                <div className="basket--info__choose--check">
+                                    <input type="checkbox" id="pickup"/>
+                                    <label className="basket--info__choose--check__pickup" htmlFor="pickup">Самовывоз</label>
+                                </div>
+                                <div className="basket--info__choose--secondCheck">
+                                    <input type="checkbox" id="delivery"/>
+                                    <label className="basket--info__choose--secondCheck__delivery" htmlFor="delivery">Доставка курьером</label>
+                                </div>
+                                <textarea name="" id="" cols="30" rows="10" placeholder="Область, город (район, село), улица, дом№, кв.№*"></textarea>
+                            </div>
+
+                            <div className="basket--info__choose--card">
+                                <div className="basket--info__choose--card__cost">
+                                    <h4>Общая сумма:</h4>
+                                    <h1>3000$</h1>
+                                </div>
+                                <div className="basket--info__choose--card__pay">
+                                    <h4>Ещё не оплачено</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ import {HiOutlineQuestionMarkCircle} from "react-icons/hi";
 import {IoIosPaper} from "react-icons/io";
 import {SlBasket} from "react-icons/sl";
 import {BsSearch} from "react-icons/bs";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
@@ -12,14 +13,15 @@ const Header = () => {
                 <div className="container">
                     <div className="header">
 
-                        <div className="header--logo">
-                            <h1>MOTION SHOP</h1>
-                        </div>
+                        <NavLink to="/">
+                            <div className="header--logo">
+                                <h1>MOTION SHOP</h1>
+                            </div>
+                        </NavLink>
 
                         <div className="header--search">
                             <input type="search"/>
                             <BsSearch className='search'/>
-
                         </div>
 
                         <div className="header--right">
@@ -31,10 +33,10 @@ const Header = () => {
                                 <IoIosPaper className='logo'/>
                                 <h1>О нас</h1>
                             </div>
-                            <div>
+                            <NavLink to="/basket">
                                 <SlBasket className='logo'/>
                                 <h1>Корзина</h1>
-                            </div>
+                            </NavLink>
                         </div>
 
                     </div>
