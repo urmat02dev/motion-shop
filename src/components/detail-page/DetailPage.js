682bd6976ad0b24be1432a12fc9f33c705611035
Urmat <urmatzhekshenov@gmail.com>
2023-02-08 12:55:17 +0600

    Basket

--- src/components/detail-page/DetailPage.js
+++ src/components/detail-page/DetailPage.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./DetailPage.scss"
 import {useParams} from "react-router-dom";
 import {backend} from "../backend";
+import {useDispatch} from "react-redux";
+import {GET_BASKET} from "../../redux/ActionTypes";
 const DetailPage = () => {
     const {id} = useParams()
+    const dispatch = useDispatch()
+    const [size , setSize] = useState(false)
+    const [color , setColor] = useState(false)
+
+
     const detail = backend[id-1]
+
+    const addToBasket = (detail) => {
+        dispatch({type:GET_BASKET, payload:detail})
+    }
     return (
         <div id="detail-page">
             <div className="container">
@@ -17,40 +28,41 @@ const DetailPage = () => {
                        <h6>{detail.price} СОМ</h6>
                        <h5>Таблица размеров:</h5>
                        <div className="detail-page--desc--size">
-                           <div>M</div>
-                           <div>XL</div>
-                           <div>XXL</div>
-                           <div>C</div>
-                           <div>S</div>
-                           <div>XS</div>
+                           <button onClick={()=> setSize(!false) } style={{color:setSize ? "#FF005C" : "white",
+                           border: size ? "2px solid #FF005C" : ""}}>XL</button>
+                           <button onClick={()=> setSize(!false)}>XXL</button>
+                           <button onClick={()=> setSize(true)}>C</button>
+                           <button onClick={()=> setSize(true)}>S</button>
+                           <button onClick={()=> setSize(true)}>XS</button>
                        </div>
                        <p>Цвет:</p>
                        <div className="detail-page--desc--color">
 
-                           <div style={{
+                           <button style={{
                                background:"black",
-                               color:"black"
-                           }}>черный</div>
-                           <div style={{
+                                  color:"black"
+                           }}>черный</button>
+                           <button style={{
                                background:"blue",
                                color:"blue"
 
-                           }}>синий</div>
-                           <div style={{
+                           }}>синий</button>
+                           <button style={{
                                background:"red",
                                color:"red"
-                           }}>красный</div>
-                           <div  style={{
+                           }}>красный</button>
+                           <button  style={{
                                background:"white",
                                color:"white"
-                           }}>белый</div>
+                           }}>белый</button>
 
 
 
                        </div>
                        <div className="detail-page--desc--btn">
-                           <button >Перейти в корзину</button>
-                           <button >Купить сейчас</button>
+                           <button onClick={() => addToBasket(detail)} className="detail-page--desc--btn--one">
+                               Добавить в корзину</button>
+                           <button className="detail-page--desc--btn--two">Купить сейчас</button>
                        </div>
                    </div>
 
