ea33f02f2807394d6c0afaf42e03849d473d8a5e
Urmat <urmatzhekshenov@gmail.com>
2023-02-08 20:42:11 +0600

    Update Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import "./Header.scss"
 import {HiOutlineQuestionMarkCircle} from "react-icons/hi";
 import {IoIosPaper} from "react-icons/io";
@@ -7,36 +7,11 @@ import {BsSearch,BsTelephoneFill} from "react-icons/bs";
 import {NavLink} from "react-router-dom";
 
 const Header = () => {
-    const [about, setAbout] = useState(false)
-
     return (
         <>
-            {
-                about ?
-                    <div>
-                    <div id="new-header">
-                        <div className="container">
-                            <div className="new-header">
-                                <div className="new-header--logo" onClick={() => setAbout(!true)}>
-                                      <div className="new-header--logo--block">
-                                            <NavLink to="/">
-                                                <h1>MOTION SHOP</h1>
-                                            </NavLink>
-                                            </div>
-                                            <div className="new-header--logo--block--right">
-                                                <BsTelephoneFill className="new-header--logo--block--right--icon"/>
-                                                <p>0888 888 888</p>
-                                            </div>
-                                        </div>
-                            </div>
-                                </div>
-                            </div>
-                    </div>
-
-                :   <div id="header">
-                        <div className="container">
-                            <div className="header">
-
+           <div id="header">
+                    <div className="container">
+                        <div className="header">
                                 <NavLink to="/">
                                     <div className="header--logo">
                                         <h1>MOTION SHOP</h1>
@@ -54,12 +29,10 @@ const Header = () => {
                                         <HiOutlineQuestionMarkCircle className='logo'/>
                                         <h1>FAQ</h1>
                                     </NavLink>
-                                    <div onClick={() => setAbout(!false)}>
                                         <NavLink to={"/about"}>
                                             <IoIosPaper className='logo'/>
                                             <h1 >О нас</h1>
                                         </NavLink>
-                                    </div>
                                     <NavLink to={"/basket"}>
                                         <SlBasket className='logo'/>
                                         <h1>Корзина</h1>
@@ -69,8 +42,6 @@ const Header = () => {
                             </div>
                         </div>
                     </div>
-
-            }
         </>
     );
 };
