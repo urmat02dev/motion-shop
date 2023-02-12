4bc95968560bdcdbb6a48665fe82a610fac772d1
AzatbekovaDev <114971065+AzatbekovaDev@users.noreply.github.com>
2023-02-12 15:08:44 +0600

    adaptation of accordion (#17)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,35 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./Header.scss"
 import {HiOutlineQuestionMarkCircle} from "react-icons/hi";
 import {IoIosPaper} from "react-icons/io";
 import {SlBasket} from "react-icons/sl";
-import {BsSearch,BsTelephoneFill} from "react-icons/bs";
+import {BsSearch} from "react-icons/bs";
 import {NavLink} from "react-router-dom";
-import Menu from "./Menu";
-
 const Header = () => {
-    const [menuActive, menublock] = useState(false)
-  const items = [{value:"FAQ",href: "faq"},{value:"О нас",href: "faq"},   {value:"Корзина",href: "faq"},]
-
     return (
         <>
            <div id="header">
                     <div className="container">
                         <div className="header">
-                                <NavLink to="/">
-                                    <div className="header--logo">
-                                        <h1>MOTION SHOP</h1>
-                                    </div>
-                                </NavLink>
-
- now_footer
-                        <nav>
-                          <div className="header--burger" onClick={() => menublock(!menuActive)}>
-                              <span/>
-                          </div>
-                        </nav>
-                        <Menu active = {menuActive} setActive = {menublock}
-                              header={"Бургер меню"} items={items}/>
                         <NavLink to="/">
                             <div className="header--logo">
                                 <h1>MOTION SHOP</h1>
@@ -38,7 +19,6 @@ const Header = () => {
                              <div className="header--search">
                                     <input type="search" placeholder="Я ищу.." />
                                     <BsSearch className='search'/>
-master
 
                                 </div>
 
@@ -56,26 +36,9 @@ master
                                         <h1>Корзина</h1>
                                     </NavLink>
                                 </div>
-
- now_footer
-                        <div className="header--right">
-                            <NavLink to={"/faq"} className="header--right__faq">
-                                <HiOutlineQuestionMarkCircle className='logo'/>
-                                <h1>FAQ</h1>
-                            </NavLink>
-                            <NavLink to={"/about"} className="header--right__faq">
-                                <IoIosPaper className='logo'/>
-                                <h1>О нас</h1>
-                            </NavLink>
-                            <NavLink to="/basket" className="header--right__faq2" >
-                                <SlBasket className='logo'/>
-                                <h1>Корзина</h1>
-                            </NavLink>
-
-                            </div>
- master
                         </div>
                     </div>
+            </div>
         </>
     );
 };
