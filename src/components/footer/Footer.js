ba9ece5f6bfbabe67200b311bdf79796217ac4a8
AzatbekovaDev <azatbekova.ya@gmail.com>
2023-02-14 16:05:44 +0600

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,40 +4,43 @@ import {SlSocialInstagram} from "react-icons/sl";
 import {FaTelegramPlane} from "react-icons/fa";
 import {AiFillFacebook} from "react-icons/ai";
 import {AiOutlineWhatsApp} from "react-icons/ai";
+
 const Footer = () => {
-        return (
-            <footer id="footer">
-                <div className="container">
-                    <div className="footer">
-                        <div className="footer--text">
-                            <div className="footer--text__one">
-                                <h1>MOTION SHOP</h1>
-                            </div>
-                            <div className="footer--text__think">
-                                <div className="footer--text__think--two">
+    return (
+        <footer id="footer">
+            <div className="container">
+                <div className="footer">
+
+                    <div className="footer--text">
+                        <div className="footer--text__one">
+                            <h1>MOTION SHOP</h1>
+                        </div>
+                        <div className="footer--text__start">
+                            <div className="footer--text__start--think">
+                                <div className="footer--text__start--think__two">
                                     <a href="#">Женщинам </a>
                                     <a href="#">Мужчинам </a>
                                     <a href="#">Детям </a>
                                 </div>
-                                <div className="footer--text__think--three">
-                                    <a href="#">Обувь</a>
-                                    <a href="#">FAQ</a>
-                                    <a href="#">О нас</a>
-                                </div>
                             </div>
-                            <div className="footer--text__number">
+                            <div className="footer--text__start--think2__three">
+                                <a href="#">Обувь</a>
+                                <a href="#">FAQ</a>
+                                <a href="#">О нас</a>
+                            </div>
+                            <div className="footer--text__start--number">
                                 <h2>Связаться с нами:</h2>
                                 <a href="tel:0222533735">+996 222 533 735</a>
                                 <a href="tel:0222533735">+996 222 533 735</a>
                                 <a href="tel:0222533735">+996 222 533 735</a>
-                                <div className="footer--text__number--icon">
-                                    <SlSocialInstagram className="footer--text__number--icon__1"/>
-                                    <FaTelegramPlane className="footer--text__number--icon__2"/>
-                                    <AiOutlineWhatsApp className="footer--text__number--icon__3"/>
-                                    <AiFillFacebook className="footer--text__number--icon__4"/>
+                                <div className="footer--text__start--number__icon">
+                                    <SlSocialInstagram className="footer--text__start--number__icon--1"/>
+                                    <FaTelegramPlane className="footer--text__start--number__icon--2"/>
+                                    <AiOutlineWhatsApp className="footer--text__start--number__icon--3"/>
+                                    <AiFillFacebook className="footer--text__start--number__icon--4"/>
                                 </div>
                             </div>
-                            <div className="footer--text__end">
+                            <div className="footer--text__start--end">
                                 <h3>Адрес</h3>
                                 <p><br/>
                                     ЖК Максимум плюс Турусбекова, <br/>
@@ -46,13 +49,14 @@ const Footer = () => {
                                     720001. Показать вход. Выходной.<br/></p>
                             </div>
                         </div>
-                        <div className="footer--end">
-                            <h1></h1>
-                            <h4>Motion-Shop - все права защищены</h4>
-                        </div>
+                    </div>
+                    <div className="footer--end">
+                        <h1></h1>
+                        <h4>Motion-Shop - все права защищены</h4>
                     </div>
                 </div>
-            </footer>
+            </div>
+        </footer>
     );
 };
 
