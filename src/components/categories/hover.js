5c7ee3a5c1ae788c0dc7051241ad96e3e002338f
Urmat <urmatzhekshenov@gmail.com>
2023-02-17 10:13:57 +0600

    Hover category

---
+++ src/components/categories/hover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './hover.css';
+
+function MyComponent() {
+    return (
+        <div className="my-component">
+            <h2>My Component</h2>
+            <p>Some text...</p>
+        </div>
+    );
+}
+
+export default MyComponent;
